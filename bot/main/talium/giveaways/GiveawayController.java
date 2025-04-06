ed7ddddbc8d91d7404012c6143c1e2d426b76817
orciument <orciument@gmail.com>
2025-04-06 15:33:11 +0200

    setup mockup endpoints

--- bot/main/talium/giveaways/GiveawayController.java
+++ bot/main/talium/giveaways/GiveawayController.java
@@ -1,8 +1,6 @@
 package talium.giveaways;
 
 import com.github.twitch4j.helix.domain.User;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,12 +9,15 @@ import talium.Out;
 import talium.giveaways.persistence.GiveawayRepo;
 import talium.giveaways.transit.*;
 
-import java.util.*;
+import java.net.URI;
+import java.util.List;
+import java.util.UUID;
+
+import static talium.TwitchBot.gson;
 
 @RestController
 @RequestMapping("/giveaway")
 public class GiveawayController {
-    private final Gson gson = new GsonBuilder().serializeNulls().create();
     private final GiveawayRepo giveawayRepo;
 
     GiveawayService giveawayService;
@@ -29,7 +30,10 @@ public class GiveawayController {
 
     @GetMapping("/templates")
     public String getGiveawayTemplates() {
-        return gson.toJson(List.of(new GiveawayTemplateDTO("BLANK", "blank")));
+        return gson.toJson(List.of(
+                new GiveawayTemplateDTO("BLANK", "blank"),
+                new GiveawayTemplateDTO("Goldkette", "goldkette"))
+        );
     }
 
     @PostMapping("/fromTemplate/{templateId}")
@@ -38,24 +42,24 @@ public class GiveawayController {
         return ResponseEntity.status(HttpStatus.CREATED).body(body);
     }
 
-    @PostMapping("/save/{gwId}")
-    public HttpStatus save(@RequestBody String body, @PathVariable UUID gwId) {
-        var giveaway = gson.fromJson(body, GiveawaySaveDTO.class);
-        System.out.println("Giveaway");
-        System.out.println(giveaway);
-        //TODO save all to DB
-        //TODO update command for GW
-        //TODO trigger possible correction of amount and ticket amounts
-        //TODO trigger update of autostart/close timers
-        return HttpStatus.CREATED;
+    @PostMapping("/create")
+    public ResponseEntity<String> save(@RequestBody String body) {
+        var toSave = gson.fromJson(body, GiveawaySaveDTO.class);
+        var dao = giveawayService.saveNewGiveaway(toSave);
+        return ResponseEntity.created(URI.create("/giveaway/get/" + dao.id())).build();
     }
 
-    //TODO open close GW
-    //TODO archive endpoint
-    //TODO draw endpoint
-    //TODO winner save endpoint
-    //TODO refund all tickets
-
+    @PostMapping("/save/{gwId}")
+    public ResponseEntity<String> save(@RequestBody String body, @PathVariable UUID gwId) {
+        var toSave = gson.fromJson(body, GiveawaySaveDTO.class);
+        var old = giveawayRepo.findById(gwId);
+        if (old.isEmpty()) {
+            ResponseEntity.status(HttpStatus.NOT_FOUND).body("Giveaway with that id not found, cannot update");
+        }
+        System.out.println("Giveaway");
+        System.out.println(toSave);
+        return ResponseEntity.ok("");
+    }
 
     @GetMapping("/get/{gwId}")
     public ResponseEntity<String> getGiveaway(@PathVariable UUID gwId) {
@@ -64,46 +68,55 @@ public class GiveawayController {
             return new ResponseEntity<>("No Giveaway with Id: " + gwId + " could be found", HttpStatus.NOT_FOUND);
         }
         var gw = entity.get();
-        var dto = new GiveawayDTO(
-                gw.id(),
-                gw.title(),
-                gw.notes(),
-                gw.createdAt().toString(),
-                gw.lastUpdatedAt().toString(),
-                gw.status(),
-                gw.command().patterns.getFirst().pattern,
-                gw.autoStart() != null ? gw.autoStart().toString() : null,
-                gw.autoEnd() != null ? gw.autoEnd().toString() : null,
-                gw.ticketCost(),
-                gw.maxTickets(),
-                gw.allowRedrawOfUser(),
-                gw.autoAnnounceWinner(),
-                gw.ticketList().stream().map(ent -> {
-                    var name = Out.Twitch.api.getUserById(ent.userId()).map(User::getId).orElse(ent.userId());
-                    return new EntryDTO(ent.userId(), name, ent.tickets());
-                }).toList(),
-                gw.winners().stream().map(win -> {
-                    var name = Out.Twitch.api.getUserById(win.userId()).map(User::getId).orElse(win.userId());
-                    return new WinnerDTO(name, win.userId(), win.rejected(), win.comment());
-                }).toList()
-        );
+        var dto = new GiveawayDTO(gw.id(), gw.title(), gw.notes(), gw.createdAt().toString(), gw.lastUpdatedAt().toString(), gw.status(), gw.command().patterns.getFirst().pattern, gw.autoStart() != null ? gw.autoStart().toString() : null, gw.autoEnd() != null ? gw.autoEnd().toString() : null, gw.ticketCost(), gw.maxTickets(), gw.allowRedrawOfUser(), gw.autoAnnounceWinner(), gw.ticketList().stream().map(ent -> {
+            var name = Out.Twitch.api.getUserById(ent.userId()).map(User::getId).orElse(ent.userId());
+            return new EntryDTO(ent.userId(), name, ent.tickets());
+        }).toList(), gw.winners().stream().map(win -> {
+            var name = Out.Twitch.api.getUserById(win.userId()).map(User::getId).orElse(win.userId());
+            return new WinnerDTO(name, win.userId(), win.rejected(), win.comment());
+        }).toList());
         return ResponseEntity.ok(gson.toJson(dto));
     }
 
     @GetMapping("/listActive")
     public String getGiveaways() {
-        return gson.toJson(giveawayRepo
-                .findAllByStatusIsNot(GiveawayStatus.ARCHIVED)
-                .stream().map(GiveawayPreview::new).toList());
+        return gson.toJson(giveawayRepo.findAllByStatusIsNot(GiveawayStatus.ARCHIVED).stream().map(GiveawayPreview::new).toList());
     }
 
-    //TODO needed: A Object to handle Giveaway AutostartTimers, schedule them, have interrupt to check if something changed on gw save,
-    // trigger action when timer up
-    //TODO concept: Do we still want to do the automatic action (autostart/close) when the status of the giveaway changed in the meantime?
-    // like, if you manually opened and closed it in between, maybe that should cancel any automatic actions?
-    // Said differently: It is probably appropriate that, if a change in status is done manually, autostart and autoclose times should be removed (null)
-    //TODO archive probably needs to be its on btn in the panel and actions, (that is just greyed out and disabled when GW is running)
+    @PostMapping("/openClose/{gwId}")
+    public ResponseEntity<String> openClose(@PathVariable UUID gwId) {
+        //TODO open close GW
+        // check is not archived
+        return ResponseEntity.ok("");
+    }
 
+    @PostMapping("/draw/{gwId}")
+    public ResponseEntity<String> draw(@PathVariable UUID gwId) {
+        //TODO draw endpoint
+        // check:
+        // - not archived
+        // - has ticket
+        // - there are sill tickets to draw
+        return ResponseEntity.ok("");
+    }
+
+    @PostMapping("/refundAllLTickets/{gwId}")
+    public ResponseEntity<String> refundAllLTickets(@PathVariable UUID gwId) {
+        //TODO refund all tickets
+        // check:
+        // - not archived
+        // - has tickets
+        return ResponseEntity.ok("");
+    }
+
+    @PostMapping("/archive/{gwId}")
+    public ResponseEntity<String> archive(@RequestParam UUID gwId) {
+        //TODO archive endpoint
+        // check is not running
+        return ResponseEntity.ok("");
+    }
+
+    //TODO winner save endpoint
 
     /* GW actions that do not originate from the panel:
         - automatic actions like: autostart/close
