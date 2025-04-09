66e6b6fea56989c95f1f2f4e27219e27adc2b835
orciument <orciument@gmail.com>
2025-04-09 23:21:42 +0200

    replace node.crypto module because it is not served when not in https

--- panel/src/components/Login/LoginPane.tsx
+++ panel/src/components/Login/LoginPane.tsx
@@ -5,7 +5,7 @@ import {PANEL_BASE_URL, TWITCH_CLIENT_ID} from "@/main.tsx";
 export default function LoginPane() {
   const redirectUri = PANEL_BASE_URL + "/twitchToken";
   const scopes = ""
-  const state = window.crypto.randomUUID();
+  const state = randomString(32);
 
   const link = new URL("https://id.twitch.tv/oauth2/authorize")
   link.searchParams.append("response_type", "token")
@@ -19,4 +19,18 @@ export default function LoginPane() {
   return <div className="login">
     <Link to={link.href}>Login with Twitch</Link>
   </div>
+}
+
+
+// const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+const characters ='`1234567890-=~!@#$%^&*()_+qwertyuiop[]\\QWERTYUIOP{}|asdfghjkl;\'ASDFGHJKL:"zxcvbnm,./ZXCVBNM<>?\n';
+
+export function randomString(length: number): string {
+  let result = '';
+  const charactersLength = characters.length;
+  for ( let i = 0; i < length; i++ ) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
 }
