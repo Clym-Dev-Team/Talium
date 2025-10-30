811d3c1ce607d6aa596f56fc5e3cf5c04451acfd
orciument <orciument@gmail.com>
2025-10-30 13:38:47 +0100

    refactor check_or_get_oauth so that it can be used at startup, and while handling; use request_new_oauth on startup

--- backend/src/commands/twitch_service.rs
+++ backend/src/commands/twitch_service.rs
@@ -1,30 +1,30 @@
-use crate::axum::AxumState;
+use crate::commands::command_executor_service::ChatMessage;
 use crate::db::ProdDB;
 use crate::service_oauth::oauth_endpoint::get_redirect_url;
-use crate::service_oauth::oauth_service::OAuthService;
 use crate::twitch::authentication::{authorization_url, refresh_token, validate_token};
-use crate::WebserverConfig;
+use crate::{FullState, L1State};
 use anyhow::{anyhow, Context};
 use asknothingx2_util::oauth::{AccessToken, ClientId};
 use serde::Deserialize;
 use sqlx::types::chrono::{DateTime, Local};
 use std::any::Any;
 use std::collections::HashMap;
-use std::sync::{Mutex, MutexGuard};
+use std::sync::Arc;
 use std::time::Instant;
-use tower::{Layer, MakeService};
-use tower_service::Service;
-use twitch_highway::chat::ChatAPI;
+use tokio::runtime::Handle;
+use tokio::sync::broadcast::Sender;
+use tokio::sync::{Mutex, RwLock};
+use tower::MakeService;
 use twitch_highway::eventsub::events::chat::ChannelChatMessage;
 use twitch_highway::eventsub::websocket::extract::{Event, State};
 use twitch_highway::eventsub::websocket::routes::{channel_chat_message, revocation, welcome};
 use twitch_highway::eventsub::websocket::{Request, Revocation, Router, Welcome};
-use twitch_highway::eventsub::{websocket, EventSubAPI, SubscriptionType};
-use twitch_highway::types::{BroadcasterId, SessionId, UserId};
+use twitch_highway::eventsub::websocket;
+use twitch_highway::types::UserId;
 use twitch_highway::users::{User, UserAPI};
 use twitch_highway::TwitchAPI;
 
-#[derive(Default, Deserialize)]
+#[derive(Default, Deserialize, Clone)]
 pub(crate) struct OauthCredential {
     pub access_token: String,
     pub refresh_token: String,
@@ -32,99 +32,58 @@ pub(crate) struct OauthCredential {
     pub expires_at: DateTime<Local>,
 }
 
-#[derive(Default, Deserialize)]
+#[derive(Default, Clone, Deserialize)]
 pub(crate) struct TwitchConfig {
     channel_name: String,
     chat_account_name: String,
     send_to: String,
-    credential: OauthCredential,
     client_id: String,
     client_secret: String,
 }
 
+struct TwitchCredentialStatus {
+    valid_checked_at: Instant,
+    was_valid_at_check: bool,
+    credential: OauthCredential,
+}
+
 pub struct TwitchService {
-    twitch_api: TwitchAPI,
-    token_validation: Mutex<(Instant, bool)>
+    twitch_api: Arc<RwLock<TwitchAPI>>,
+    token_validation: Mutex<TwitchCredentialStatus>,
+    config: TwitchConfig,
+    l1: Arc<L1State>,
 }
 
 impl TwitchService {
-    async fn check_or_get_oauth(mut lock: MutexGuard<'_, (Instant, bool)>, db: ProdDB, oauth_service: OAuthService, webserver_config: WebserverConfig, twitch_config: TwitchConfig) -> Result<OauthCredential, anyhow::Error> {
-        //TODO check lock
-        //TODO validate
-        //TODO refresh
-        //TODO request new oauth
-        const FINAL_ERROR: &str = "Could not get new oauth token, bad credentials, needs reauthentication";
-        let credential = twitch_config.credential;
-        if lock.0.elapsed() < std::time::Duration::from_secs(15 * 60) {
-            return if lock.1 {
-                Ok(credential)
-            } else {
-                Err(anyhow!(FINAL_ERROR))
-            }
-        }
-        let mut errors = vec![];
-        match validate_token(credential.access_token.as_str()).await {
-            Ok(Some(_validation)) => {
-                *lock = (Instant::now(), true);
-                return Ok(credential)
-            },
-            Err(e) =>errors.push(e),
-            Ok(None) => {}
-        };
-        match refresh_token(credential.refresh_token, twitch_config.client_id.as_str(), twitch_config.client_secret).await {
-            Ok(Some(refreshed)) => {
-                //TODO save to db
-                *lock = (Instant::now(), true);
-                return Ok(refreshed.into())
-            }
-            Err(e) =>errors.push(e),
-            Ok(None) => {}
-        }
-        //TODO we cant do this here, our caller needs to do this, because one time we need to block, and don't have a TwitchService object,
-        // and another time we should not block and should hopefully be able to get a TwitchService object
-        // Handle::current().spawn(async move {
-        //     let (url, state) = authorization_url(twitch_config.client_id.as_str(), get_redirect_url(webserver_config.panel_base_url.to_string(), "twitch"));
-        //     let code = oauth_service.new_oauth_request("twitch", twitch_config.chat_account_name, url, state);
-        //     //TODO save to db
-        //     //TODO update access token in twitch_api
-        // });
-        *lock = (Instant::now(), false);
-        //TODO add errors
-        Err(anyhow!(FINAL_ERROR))
-    }
-
-    pub async fn new(db: ProdDB, oauth_service: OAuthService, webserver_config: WebserverConfig, twitch_config: TwitchConfig) -> Result<TwitchService, anyhow::Error> {
+    pub async fn new(l1: Arc<L1State>, twitch_config: TwitchConfig) -> Result<TwitchService, anyhow::Error> {
+        let cred_from_db: OauthCredential = ();
         //TODO get credentials from db (our caller does that)
-        let token = if let Some(refreshed) = refresh_token(twitch_config.credential.access_token.as_str(), twitch_config.client_id.as_str(), twitch_config.client_secret.as_str())
-            .await
-            .context("Unable to start twitch input because oauth could not be refreshed")? {
-            refreshed
-        } else {
-            let (url, state) = authorization_url(twitch_config.client_id.as_str(), get_redirect_url(webserver_config.panel_base_url.to_string(), "twitch"));
-            let code = oauth_service.new_oauth_request("twitch", twitch_config.chat_account_name, url, state);
-            let token = refresh_token(code.as_str(), twitch_config.client_id.as_str(), twitch_config.client_secret.as_str()).await
-                .context("Unable to get oauth tokens for auth flow code")?
-                .context("Auth flow code invalid, unable to start twitch client, could not get oauth token")?;
-            //TODO save to db
-            token
+        let oauth = match Self::check_or_get_oauth(&cred_from_db, &l1.prod_db, &twitch_config).await {
+            Ok(oauth) => oauth,
+            Err(e) => {
+                //TODO log err
+                TwitchCredentialStatus {
+                    was_valid_at_check: true,
+                    valid_checked_at: Instant::now(),
+                    credential: Self::request_new_oauth(l1, twitch_config.clone()).await
+                }
+            },
         };
         let api = TwitchAPI::new(
-            AccessToken::from("your_access_token"),
-            ClientId::from("your_client_id"),
+            AccessToken::from(oauth.credential.access_token.as_str()),
+            ClientId::from(twitch_config.client_id.as_str()),
         );
 
-        //TODO setup websocket
-        //TODO setup websocket subscriptions
         //TODO setup periodic token refresher
         //TODO refresh and save token on shutdown
-        let userId = UserId::from("userId");
-        let d = api.get_users().ids(&[userId.clone()]).logins(&["test"]).build().json().await.unwrap().data;
-        let target_channel = BroadcasterId::from("");
 
-        let d = api.send_chat_message(&target_channel, &userId, "").for_source_only(true).send().await;
-        let d = api.websocket_subscription(SubscriptionType::ChannelChatMessage, SessionId::from("")).send().await.unwrap();
+        // let userId = UserId::from("userId");
+        // let d = api.get_users().ids(&[userId.clone()]).logins(&["test"]).build().json().await.unwrap().data;
+        // let target_channel = BroadcasterId::from("");
 
-        todo!()
+        // let d = api.send_chat_message(&target_channel, &userId, "").for_source_only(true).send().await;
+        // let d = api.websocket_subscription(SubscriptionType::ChannelChatMessage, SessionId::from("")).send().await.unwrap();
+
         // AccessToken Validation
         // Problem 1: This library does not tell us what really happened. We will get a 401 from twitch,
         // but that can have more reasons than just a timed out token.
@@ -151,20 +110,67 @@ impl TwitchService {
         // One additional benefit is that we are not constantly checking the validity for each user, only when they actually do something.
         //
         // This would mean that our session are always in schrödinger-state of validity (although they are currently anyway)
+        Ok(TwitchService {
+            twitch_api: Arc::new(RwLock::new(api)),
+            token_validation: Mutex::new(oauth),
+            config: twitch_config,
+            l1,
+        })
     }
 
-    pub async fn start_websocket(state: AxumState) {
-        async fn process_messages(state: State<AxumState>, message: Event<ChannelChatMessage>) {
-            //TODO
-            //TODO deduplicate message (ids) with ringbuffer
+    async fn check_or_get_oauth(cred: &OauthCredential, db: &ProdDB, twitch_config: &TwitchConfig) -> Result<TwitchCredentialStatus, anyhow::Error> {
+        const FINAL_ERROR: &str = "Could not get new oauth token, bad credentials, needs reauthentication";
+        // is first thread, do validation/refreshing
+        let mut errors = vec![];
+        match validate_token(cred.access_token.as_str()).await {
+            Ok(Some(_validation)) => {
+                return Ok(TwitchCredentialStatus {
+                    valid_checked_at: Instant::now(),
+                    was_valid_at_check: true,
+                    credential: cred.clone(),
+                })
+            },
+            Err(e) =>errors.push(e),
+            Ok(None) => {}
+        };
+        match refresh_token(cred.refresh_token.as_str(), twitch_config.client_id.as_str(), twitch_config.client_secret.as_str()).await {
+            Ok(Some(refreshed)) => {
+                //TODO save to db
+                return Ok(TwitchCredentialStatus {
+                    valid_checked_at: Instant::now(),
+                    was_valid_at_check: true,
+                    credential: refreshed.into(),
+                })
+            }
+            Err(e) =>errors.push(e),
+            Ok(None) => {}
         }
-        async fn process_welcome(state: State<AxumState>, welcome: Event<Welcome>) {
+        //TODO add errors
+        Err(anyhow!(FINAL_ERROR))
+    }
+
+    async fn request_new_oauth(l1: Arc<L1State>, twitch_config: TwitchConfig) -> OauthCredential {
+        let (url, state) = authorization_url(twitch_config.client_id.as_str(), get_redirect_url("".to_string(), "twitch"));
+        //TODO make the url kida a builder, that gets evaluated when the api is actually called
+        let code = l1.oauth_service.new_oauth_request("twitch", twitch_config.chat_account_name.as_str(), url, state);
+
+        let refreshed = refresh_token(code, twitch_config.client_id.as_str(), twitch_config.client_secret.as_str()).await;
+        //TODO save to db
+        todo!()
+    }
+
+    pub async fn start_websocket(state: Arc<FullState>, commands_sender: Sender<Box<ChatMessage>>) {
+        async fn process_messages(state: State<Arc<FullState>>, message: Event<ChannelChatMessage>) {
+            //TODO deduplicate message (ids) with ringbuffer
+            //TODO push into sender
+        }
+        async fn process_welcome(state: State<Arc<FullState>>, welcome: Event<Welcome>) {
             // here we need to use the sessionId from the welcome message and set it into the client for later use
             // after that we need to register all the topic we need to our sessionId, for that we would need to use the client
         }
-        async fn process_revocation(state: State<AxumState>, welcome: Event<Revocation>) {}
+        async fn process_revocation(state: State<Arc<FullState>>, welcome: Event<Revocation>) {}
 
-        let twitch_router = <twitch_highway::eventsub::websocket::Router as MakeService<(), Request>>::into_service(Router::<AxumState>::new()
+        let twitch_router = <Router as MakeService<(), Request>>::into_service(Router::<Arc<FullState>>::new()
             .route(welcome(process_welcome))
             .route(revocation(process_revocation))
             .route(channel_chat_message(process_messages))
@@ -177,11 +183,14 @@ impl TwitchService {
 
     pub async fn get_user_by_id(&self, id: String) -> Result<Option<User>, anyhow::Error> {
         let ids = [UserId::from(id)];
-        let req = || {
+        let req = || async {
             self.twitch_api
+                .read()
+                .await
                 .get_users()
                 .ids(&ids)
                 .json()
+                .await
         };
         let mut res = self.handle_401_and_retry(req).await.context("failed to get user by id")?;
         Ok(res.data.pop())
@@ -194,26 +203,32 @@ impl TwitchService {
     {
         match req().await {
             Err(e) if e.is_api() && e.message().is_some_and(|t1| t1.starts_with("HTTP 401")) => {
-                //TODO handle unwrap
-                let lock = self.token_validation.lock().unwrap();
-                //TODO use new fn:
-                // Self::check_or_get_oauth(lock, )
-                if lock.0.elapsed() > std::time::Duration::from_secs(15 * 60) {
-                    match refresh_token(self.twitch_api.access_token()).await? {
-                        Ok(t) => {
-                            //TODO save new token in db
-                            req().await.context("Retry also failed")
+                let mut cred_lock = self.token_validation.lock().await;
+                if cred_lock.valid_checked_at.elapsed() > std::time::Duration::from_secs(15 * 60) {
+                     match Self::check_or_get_oauth(&cred_lock.credential, &self.l1.prod_db, &self.config).await {
+                        Ok(d) => {
+                            let mut api_lock = self.twitch_api.write().await;
+                            *api_lock = api_lock.clone().set_access_token(AccessToken::from(d.credential.access_token.clone()));
+                            *cred_lock = d;
                         },
                         Err(e) => {
-                            //TODO request new auth in new thread
-                            Err(e).context("Failed to refresh oauth token, failing current request")
-                        },
-                    }
-                } else if lock.1 {
-                    req().await.context("Retry also failed")
-                } else {
-                    Err(e).context("Credentials Bad, not retrying")
+                            cred_lock.valid_checked_at = Instant::now();
+                            cred_lock.was_valid_at_check = false;
+                            let config = self.config.clone();
+                            let l1 = self.l1.clone();
+                            Handle::current().spawn(async {
+                                let credential = TwitchService::request_new_oauth(l1, config);
+                                //TODO update state
+                            });
+                            return Err(e).context("Credentials Bad, not retrying");
+                        }
+                    };
+                };
+                if !cred_lock.was_valid_at_check {
+                    return Err(anyhow!("Could not get new oauth token, bad credentials, needs reauthentication"));
                 }
+                drop(cred_lock);
+                req().await.context("Retry also failed")
             },
             Err(e) => Err(e.into()),
             Ok(r) => Ok(r),
